8e4d7c3c0294aee5f2a19ff6f7c30fb2cc28ef25
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-02-19 12:59:21 +0400

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,18 +1,6 @@
 class MissingEnvironmentVariableError(Exception):
     """Исключение для отсутствующих переменных окружения."""
-    pass
 
 
-class APIRequestError(Exception):
-    """Исключение для ошибок запроса к API."""
-    pass
-
-
-class UnknownHomeworkStatusError(Exception):
-    """Исключение для неизвестного статуса домашней работы."""
-    pass
-
-
-class TelegramSendMessageError(Exception):
-    """Исключение для ошибок отправки сообщения в Telegram."""
-    pass
+class InvalidResponseCodeError(Exception):
+    """Исключение для неверного кода ответа API."""
